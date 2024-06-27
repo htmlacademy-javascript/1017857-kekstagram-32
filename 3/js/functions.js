42d7c36a1714aa0b7da71cbd7427682af6e85254
Keks <keks@htmlacademy.ru>
2024-06-27 22:43:30 +0300

    :heavy_check_mark: Сборка #3

---
+++ 3/js/functions.js
@@ -0,0 +1,45 @@
+/**
+ * Функция для проверки длины строки.
+ * @param {string} word - проверяемая строка
+ * @param {number} maxWordLength - максимальная длина строки
+ * @return {boolean} - true, если длина строки не превышает максимальную длину
+ */
+const checkWordLength = (word, maxWordLength) => word.length <= maxWordLength;
+
+
+/**
+ * Функция для проверки, является ли строка палиндромом.
+ * @param {string} word - проверяемая строка
+ * @return {boolean} - true, если строка является палиндромом
+ */
+const isPalindrome = (word) => {
+  const normalWord = word.toLowerCase().replaceAll(' ', '');
+  let reverseWord = '';
+  for (let i = normalWord.length - 1; i >= 0; i--) {
+    reverseWord += normalWord[i];
+  }
+  return reverseWord === normalWord;
+};
+
+
+/**
+ * Функция принимает строку, извлекает содержащиеся в ней цифры от 0 до 9 и возвращает их в виде целого положительного числа.
+ * @param {string, number} value - принимаемая строка.
+ * @return {number} - результат вычисления в виде целого положительного числа. Если в строке нет ни одной цифры, функция возвращает NaN
+ */
+const getNumber = (value) => {
+  const string = value.toString();
+  let result = '';
+  for (let i = 0; i < string.length; i++) {
+    const char = parseInt(string.at(i), 10);
+    if (!Number.isNaN(char)) {
+      result += char;
+    }
+  }
+  return parseInt(result, 10);
+};
+
+
+checkWordLength('проверяемая строка', 20);
+isPalindrome('топот');
+getNumber('2023 год');
