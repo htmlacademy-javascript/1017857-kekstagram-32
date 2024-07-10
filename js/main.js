178cc02754a027b98b7efdfa11a7a33632ca462d
niknov80 <n.novozhilov80@gmail.com>
2024-07-10 22:47:39 +0300

    Манипуляции с DOM

--- js/main.js
+++
@@ -1,7 +0,0 @@
-import {createPhotoData} from './data.js';
-
-const PHOTOS_COUNT = 25;
-const photosData = Array.from({length: PHOTOS_COUNT}, createPhotoData);
-
-// eslint-disable-next-line no-console
-console.log(photosData);
