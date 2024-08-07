ada5345b6a7b56cd531187366a89b7a773a62ce5
Keks <keks@htmlacademy.ru>
2024-08-07 15:42:04 +0300

    :heavy_check_mark: Сборка #9

---
+++ 9/js/picture.mjs
@@ -0,0 +1,32 @@
+import {openBigPicture} from './big-picture.mjs';
+
+const pictureListElement = document.querySelector('.pictures');
+const pictureTemplate = document.querySelector('#picture').content.querySelector('.picture');
+const pictureListFragment = document.createDocumentFragment();
+
+/**
+ * Функция создает изображение и добавляет его в DocumentFragment списка изображений
+ * @param {Object} pictureData - объект с данными изображения
+ */
+const createPicture = (pictureData) => {
+  const pictureElement = pictureTemplate.cloneNode(true);
+  pictureElement.querySelector('.picture__img').setAttribute('src', pictureData.url);
+  pictureElement.querySelector('.picture__img').setAttribute('alt', pictureData.description);
+  pictureElement.querySelector('.picture__comments').textContent = pictureData.comments.length;
+  pictureElement.querySelector('.picture__likes').textContent = pictureData.likes;
+  pictureElement.addEventListener('click', () => {
+    openBigPicture(pictureData);
+  })
+  pictureListFragment.appendChild(pictureElement);
+};
+
+/**
+ * Функция создает список из случайных изображений пользователей и выводит его на странице
+ * @param {Array} photoData - массив объектов с данными изображений
+ */
+const createPictureList = (photoData) => {
+  photoData.forEach((item) => createPicture(item));
+  pictureListElement.appendChild(pictureListFragment);
+};
+
+export {createPictureList};
---
+++ 9/js/user-modal.js
@@ -0,0 +1,68 @@
+import {isEscapeKey} from './utils.mjs';
+import {resetEffect} from './effect.js';
+import {resetMessageAndHashtagText, isValidComment} from './comment-validation.js';
+
+const imageUploadForm = document.querySelector('#upload-select-image');
+const userForm = document.querySelector('.img-upload__overlay');
+const userFormCancel = document.querySelector('#upload-cancel');
+const hashtagElement = imageUploadForm.querySelector('.text__hashtags');
+const userCommentElement = imageUploadForm.querySelector('.text__description');
+const uploadFileElement = document.querySelector('#upload-file');
+const userPicture = document.querySelector('.img-upload__preview img');
+
+/**
+ * Закрытие формы загрузки изображения
+ */
+const closeUserForm = () => {
+  userForm.classList.add('hidden');
+  document.querySelector('body').classList.remove('modal-open');
+  document.removeEventListener('keydown', onDocumentKeydown);
+  uploadFileElement.value = '';
+  resetMessageAndHashtagText();
+};
+
+/**
+ * Открытие формы загрузки изображения
+ */
+const openUserForm = () => {
+  userForm.classList.remove('hidden');
+  document.querySelector('body').classList.add('modal-open');
+  document.addEventListener('keydown', onDocumentKeydown);
+
+  resetEffect(userPicture);
+  isValidComment();
+};
+
+/**
+ * Функция добавляет обработчики события на форму загрузки изображения
+ */
+const addEventListenersToForm = () => {
+  userFormCancel.addEventListener('click', () => {
+    closeUserForm();
+  });
+  hashtagElement.addEventListener('focus', () => {
+    document.removeEventListener('keydown', onDocumentKeydown);
+  });
+  hashtagElement.addEventListener('blur', () => {
+    document.addEventListener('keydown', onDocumentKeydown);
+  });
+  userCommentElement.addEventListener('focus', () => {
+    document.removeEventListener('keydown', onDocumentKeydown);
+  });
+  userCommentElement.addEventListener('blur', () => {
+    document.addEventListener('keydown', onDocumentKeydown);
+  });
+};
+
+/**
+ * Обработчик события на закрытие формы загрузки изображения по кнопке Esc
+ * @param {object} evt - объект события
+ */
+function onDocumentKeydown(evt) {
+  if (isEscapeKey(evt)) {
+    evt.preventDefault();
+    closeUserForm();
+  }
+}
+
+export {openUserForm, closeUserForm, onDocumentKeydown, addEventListenersToForm};
